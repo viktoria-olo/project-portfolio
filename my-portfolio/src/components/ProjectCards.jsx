56b9c0cf90031443d36238a49e354a15e8003d00
Viktoria Olofsson <viktoriaolofsson@gmail.com>
2024-03-26 21:00:42 +0100

    project images + links added

--- my-portfolio/src/components/ProjectCards.jsx
+++ my-portfolio/src/components/ProjectCards.jsx
@@ -1,39 +1,59 @@
 import { Button } from "./Button";
+import { Image } from "../components/Image";
 
 export const ProjectCards = ({ projects }) => {
-  const netlifyLinks = {
-    756390333: "https://chipper-starburst-234ce1.netlify.app/", // Chatbot
-    774386088: "https://flourishing-mooncake-e332de.netlify.app/", // Happy Thoughts
-    759009670: "https://sparkling-cajeta-657abb.netlify.app/", // Library
-    768010316: "https://heroic-crisp-8a05fa.netlify.app/", // Music Releases
-    749447525: "https://imaginative-croissant-f2713d.netlify.app/", // News Site
-    752195534: "https://cosmic-ganache-90f1e4.netlify.app/", // Pizza ordering
-    777377015: null, // Project Portfolio
-    770952235: "https://ornate-basbousa-d697b8.netlify.app/", // Survey
-    763691885: "https://marvelous-empanada-370879.netlify.app/", // Weather App
+  const imageLinks = {
+    774386088: "../../public/images/happy-thoughts.jpg", // Happy Thoughts
+    759009670: "../../public/images/books.jpg", // Library
+    768010316: "../../public/images/music-record.jpg", // Music Releases
+    770952235: "../../public/images/survey-clipboard.jpg", // Survey
+    763691885: "../../public/images/weather.jpg", // Weather App
   };
+  /*
+  <Button
+      icon="../../public/icons/Live-Demo-Button.svg"
+      altText="view the page"
+      link={netlifyLinks[project.id]}
+     />*/
+
   return (
     <>
       {projects && (
         <div>
-          {projects.map((project) => (
-            <div key={project.id}>
-              <h2>{project.name.replace("project", "").replace(/-/g, " ")}</h2>
-              <p>{project.description}</p>
-              <Button
-                icon="../../public/icons/View-Code-Button.svg"
-                altText="view the code button"
-                link={project.html_url}
-              />
-              {netlifyLinks[project.id] && (
-                <Button
-                  icon="../../public/icons/Live-Demo-Button.svg"
-                  altText="view the page"
-                  link={netlifyLinks[project.id]}
-                />
-              )}
-            </div>
-          ))}
+          {projects.map((project) => {
+            if (
+              project.id !== 756390333 &&
+              project.id !== 749447525 &&
+              project.id !== 752195534 &&
+              project.id !== 777377015
+            ) {
+              return (
+                <div key={project.id}>
+                  <Image
+                    className="project-image"
+                    link={imageLinks[project.id]}
+                    altText="project image"
+                  />
+                  <h2>
+                    {project.name.replace("project", "").replace(/-/g, " ")}
+                  </h2>
+                  <p>{project.description}</p>
+                  <p>{project.topics}</p>
+                  <Button
+                    icon="../../public/icons/View-Code-Button.svg"
+                    altText="view the code button"
+                    link={project.html_url}
+                  />
+                  <Button
+                    icon="../../public/icons/Live-Demo-Button.svg"
+                    altText="view the page"
+                    link={project.homepage}
+                  />
+                </div>
+              );
+            }
+            return null;
+          })}
         </div>
       )}
     </>
